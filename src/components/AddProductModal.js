bb2a8b434939b3e9d5ee6d2000f584158682b4cf
Raheel Arif <raheelarif@192.168.10.13>
2024-08-13 16:29:49 +0500

    variation ids

--- src/components/AddProductModal.js
+++ src/components/AddProductModal.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Modal, Form, Input, InputNumber, Button, Upload } from 'antd';
+import { Modal, Form, Input, InputNumber, Button, Space } from 'antd';
 import { addProduct } from '../app/slices/productsSlice';
-import { UploadOutlined } from '@ant-design/icons';
+import { PlusOutlined, MinusCircleOutlined } from '@ant-design/icons';
 
 const AddProductModal = ({ visible, onClose }) => {
     const dispatch = useDispatch();
@@ -26,7 +26,7 @@ const AddProductModal = ({ visible, onClose }) => {
                 layout="vertical"
                 onFinish={onFinish}
             >
-                <Form.Item
+                {/* <Form.Item
                     name="sku"
                     label="SKU"
                     rules={[{ required: true, message: 'Please input the SKU!' }]}
@@ -39,7 +39,7 @@ const AddProductModal = ({ visible, onClose }) => {
                     rules={[{ required: true, message: 'Please input the GTIN!' }]}
                 >
                     <InputNumber style={{ width: '100%' }} />
-                </Form.Item>
+                </Form.Item> */}
                 <Form.Item
                     name="title"
                     label="Title"
@@ -54,13 +54,13 @@ const AddProductModal = ({ visible, onClose }) => {
                 >
                     <Input.TextArea />
                 </Form.Item>
-                <Form.Item
+                {/* <Form.Item
                     name="salePrice"
                     label="Sale Price"
                     rules={[{ required: true, message: 'Please input the sale price!' }]}
                 >
                     <InputNumber style={{ width: '100%' }} />
-                </Form.Item>
+                </Form.Item> */}
                 <Form.Item
                     name="originalPrice"
                     label="Original Price"
@@ -75,7 +75,7 @@ const AddProductModal = ({ visible, onClose }) => {
                 >
                     <Input />
                 </Form.Item>
-                <Form.Item
+                {/* <Form.Item
                     name="market"
                     label="Market"
                     rules={[{ required: true, message: 'Please input the market!' }]}
@@ -88,7 +88,7 @@ const AddProductModal = ({ visible, onClose }) => {
                     rules={[{ required: true, message: 'Please input the brand!' }]}
                 >
                     <Input />
-                </Form.Item>
+                </Form.Item> */}
                 <Form.Item
                     name="image"
                     label="Image URL"
@@ -96,14 +96,6 @@ const AddProductModal = ({ visible, onClose }) => {
                 >
                     <Input />
                 </Form.Item>
-                {/* <Form.Item
-                    name="extraImages"
-                    label="Extra Images"
-                >
-                    <Upload multiple listType="picture" action="#">
-                        <Button icon={<UploadOutlined />}>Upload</Button>
-                    </Upload>
-                </Form.Item> */}
                 <Form.Item
                     name="quantity"
                     label="Quantity"
@@ -111,6 +103,32 @@ const AddProductModal = ({ visible, onClose }) => {
                 >
                     <InputNumber style={{ width: '100%' }} />
                 </Form.Item>
+                
+                <Form.List name="variation_ids">
+                    {(fields, { add, remove }) => (
+                        <>
+                            {fields.map((field, index) => (
+                                <Space key={field.key} align="baseline" style={{ display: 'flex', marginBottom: 8 }}>
+                                    <Form.Item
+                                        {...field}
+                                        name={[field.name]}
+                                        fieldKey={[field.fieldKey]}
+                                        rules={[{ required: true, message: 'Please input a variation ID!' }]}
+                                    >
+                                        <Input placeholder="Variation ID" />
+                                    </Form.Item>
+                                    <MinusCircleOutlined onClick={() => remove(field.name)} />
+                                </Space>
+                            ))}
+                            <Form.Item>
+                                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                                    Add Variation ID
+                                </Button>
+                            </Form.Item>
+                        </>
+                    )}
+                </Form.List>
+                
                 <Form.Item>
                     <Button type="primary" htmlType="submit">
                         Add Product
